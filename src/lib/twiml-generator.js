68e5dbc5d5167baa1daa0249791bfcd97b1549ab
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-09-14 13:15:30 -0500

    --

---
+++ src/lib/twiml-generator.js
@@ -0,0 +1,16 @@
+var MessagingResponse = require('twilio').twiml.MessagingResponse
+
+
+var notFound = function() {
+  var resp = new MessagingResponse();
+  resp.message('We did not find the shelter you\'re looking for');
+  return resp;
+};
+
+var singleShelter = function(shelter) {
+  var resp = new MessagingResponse();
+  var message = resp.message();
+  message.body(`${shelter.name}\n${shelter.notes}\n${shelter.lat}`);
+  message.media(shelter.imageUrl);
+  return resp;
+};
