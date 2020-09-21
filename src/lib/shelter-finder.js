d1992aaa39de03cafa298e31201573e681bcac10
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-09-21 16:18:42 -0500

    --

--- src/lib/shelter-finder.js
+++ src/lib/shelter-finder.js
@@ -9,33 +9,6 @@ const findByName = function(name, callback) {
 
 
 
-// Shelter.findOne({ 'name': 'Black Gap Shelter' }, 'name notes', function (err, shelter) {
-//   if (err) return handleError(err);
-//   // Prints "Space Ghost is a talk show host".
-//   console.log(shelter.name, shelter.notes);
-// });
-
-
-
-
-
-
-// Shelter.findOne({name: 'Black Gap Shelter'}, 'notes').exec();
-
-
-
-// Shelter.find().byName('Black Gap Shelter').exec((err, shelters) => {
-//     console.log(shelters);
-//   });
-
-// shelterSchema.query.byName = function(name) {
-//     return this.where({ name: new RegExp(name, 'i') })
-//   };
-
-
-// Shelter.find().byName('Black Gap Shelter').exec((err, shelter) => {
-//     console.log(animals);
-//   });
 
 
 
