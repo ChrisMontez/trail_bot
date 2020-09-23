c8dcd23e4bdea680e681bd6812a49a7f641491bf
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-09-23 00:04:09 -0500

    chained promises to db callback function. Node error 9156 MAY be fixed. The bot responds with notFound function. Response is wrong but fast

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,4 +4,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/appalachian_shelters', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true
-})
+}).then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
